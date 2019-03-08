d7792968c448c62d75940b62d4c1931d0b1c3920
Danooshan Sureshkumar <danooshan0919@gmail.com>
2019-03-08 16:26:13 -0500

    Added documentation and functionality to TicketManager/checkNegativeTickets

--- noLabToday/TicketManager.java
+++ noLabToday/TicketManager.java
@@ -11,13 +11,39 @@ package noLabToday;
 
 public class TicketManager {
   public String eventName;
-  public double price;
+  public double price; 
   public String String;
   public int numTickets;
 
-  public void checkNegativeTickets() {}
+	/*
+		This function is used to check the constraint of having negative tickets, and updates numTickets remaining for the specific event.
+		@params: int ticketsPurchased - The number of tickets purchased
+		@returns: True if: Tickets are greater or equal to zero
+							False if: Tickets are less than zero
+	*/
+  public boolean checkNegativeTickets(int ticketsPurchased) {
+		int remainingTickets = numTickets - ticketsPurchased;
+		if (remainingTickets >= 0){
+			numTickets -= ticketsPurchased;
+			return true;
+		}
+		else {
+			System.out.println("ERROR:  Insufficient tickets available for purchase!");
+			return false;
+		}
+	}
 
-  public void getTicketInformation() {}
+	/*
+		TODO: Still working on dis
+	*/
+  public void getTicketInformation() {
+		String message = "Eventname = " + eventName + ",\nCost = " + String.valueOf(price) + ",\nTickets remaining = " + String.valueOf(numTickets);
+		System.out.println(message);
+	}
 
+
+	/*
+		TODO: Still working on dis
+	*/
   public void checkDuplicateEvent() {}
 }
