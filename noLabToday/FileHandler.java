41aca1042c38f055c2aa493df71ea9f0abc5a466
Matthew Wierzbicki <mateuszwierzbicki97@gmail.com>
2019-03-09 14:36:35 -0500

    Added Comments and a few more methods
    Added Comments and a few more methods related to verification

--- noLabToday/FileHandler.java
+++ noLabToday/FileHandler.java
@@ -12,20 +12,35 @@ import java.nio.*;
 * @version 0.1
 * @since 2019-03-05
 */
-
+ 
 public class FileHandler {
+ 
   public String fileName;
-
-  public List<String> updateTickets() {
+  
+  /**
+   * The Update tickets method updates the ticket values to reflect the previous days transactions.
+   * @return returns a list of strings with the new ticket lines.
+   */
+public List<String> updateTickets() {
     List<String> ticketList = new ArrayList<String>();
     return ticketList;
   }
 
-  public List<String> updateUsers() {
+  /**
+   * The Update users method updates the user values to reflect the previous days transactions.
+   * @return
+   */
+public List<String> updateUsers() {
     List<String> userList = new ArrayList<String>();
     return userList;
   }
-  public static List<String> readFile(String fileName) throws IOException {
+  /**
+   * reads the file and stores the contents in a list of strings
+ * @param fileName the name of the file in string format
+ * @return returns a list of strings (the file as a list of strings)
+ * @throws IOException when the file cannot be found or any issues with the i/o occur
+ */
+public static List<String> readFile(String fileName) throws IOException {
 	  InputStream is = new FileInputStream(fileName);
 	  BufferedReader buf = new BufferedReader(new InputStreamReader(is));
 
@@ -40,10 +55,15 @@ public class FileHandler {
 		  }
 
 	  }
-	  System.out.println(fileAsString.toString());
 	  return fileAsString;
   }
-  public boolean verifyFileFormat(String fileName) throws java.io.IOException
+  /**
+ * Verifies the format of the user file 
+ * @param fileName the name of the file in string format
+ * @return returns a list of strings (the file as a list of strings)
+ * @throws java.io.IOException when the file cannot be found or any issues with the i/o occur
+ */
+public boolean verifyUserFileFormat(String fileName) throws java.io.IOException
   {
 
 	  List<String> lineList = readFile(fileName);
@@ -59,26 +79,146 @@ public class FileHandler {
 			  type = line.substring(16,18);
 			  credit = line.substring(19,28);
 
-			  System.out.println(line.length());
-			  System.out.println(type);
-
 			  if(line.length() != 28)
 			  {
 				  check = false;
 			  }
-	    	  else if (type != "AA" && type != "FS" && type != "BS" && type != "SS")
+	    	  else if (!type.equals("AA") && !type.equals("FS") && !type.equals("BS") && !type.equals("SS"))
 			  {
 		    	 check = false;
 			  }
+	    	  else if (Double.parseDouble(credit) > 999999.99 || Double.parseDouble(credit) < 0)
+	    	  {
+	    		  check = false;
+	    	  }
 		  }
 
       }
 
 	  return check;
   }
+/**
+* Verifies the format of the stock file 
+* @param fileName the name of the file in string format
+* @return returns a list of strings (the file as a list of strings)
+* @throws java.io.IOException when the file cannot be found or any issues with the i/o occur
+*/
+public boolean verifyStockFileFormat(String fileName) throws java.io.IOException
+  {
 
-  public void readAllFiles(String fileNames[]) {
+	  List<String> lineList = readFile(fileName);
+	  String eventName = "";
+	  String username = "";
+	  String amountOfTickets = "";
+	  String ticketPrice = "";
+	  boolean check = true;
+	  for (String line : lineList)
+	  {
+		  if(line != null)
+		  {
+			  eventName = line.substring(0,26);
+			  username = line.substring(26,42);
+			  amountOfTickets = line.substring(42,46);
+			  ticketPrice = line.substring(46,52);
+			  
+			  if(line.length() != 52)
+			  {
+				  check = false;
+			  }
+	    	  else if (Double.parseDouble(ticketPrice) > 999.99 || Double.parseDouble(ticketPrice) < 0)
+	    	  {
+	    		  check = false;
+	    	  }
+	    	  else if (Integer.parseInt(amountOfTickets.trim()) > 100 || Integer.parseInt(amountOfTickets.trim()) < 0)
+	    	  {
+	    		  check = false;
+	    	  }
+		  }
+      }
+	  return check;
+  }
+  /**
+ * @param fileName
+ * @return
+ * @throws java.io.IOException
+ */
+public boolean verifyTransFileFormat(String fileName) throws java.io.IOException
+  {
 
-
+	  List<String> lineList = readFile(fileName);
+	  boolean check = true;
+	  String transCode = "";
+	  for (String line : lineList)
+	  {
+		  if(line != null)
+		  {
+			  transCode = line.substring(0, 2);
+			  if(transCode.trim().equals("00"))
+			  {
+				  if(line.length() != 2)
+				  {
+					  check = false;
+				  }
+			  }
+			  else if(transCode.trim().equals("01"))
+			  {
+				  if(line.length() != 31)
+				  {
+					  check = false;
+				  }
+			  }
+			  else if(transCode.trim().equals("02"))
+			  {
+				  if(line.length() != 31)
+				  {
+					  check = false;
+				  }
+			  }
+			  else if(transCode.trim().equals("03"))
+			  {
+				  if (line.length() != 55)
+				  {
+					  check = false;
+				  }
+			  }
+			  else if(transCode.trim().equals("04"))
+			  {
+				  if (line.length() != 55)
+				  {
+					  check = false;
+				  }
+			  }
+			  else if(transCode.trim().equals("05"))
+			  {
+				  if(line.length() != 44)
+				  {
+					  check = false;
+				  }
+			  }
+			  else if(transCode.trim().equals("06"))
+			  {
+				  if(line.length() != 31)
+				  {
+					  check = false;
+				  }
+			  }
+			  else
+			  {
+				  check = false;
+			  }
+		  }
+      }
+	  return check;
+  }
+/**
+ * reads the file and stores the contents in a list of strings
+* @param fileName the name of the file in string format
+* @return returns a list of strings (the file as a list of strings)
+* @throws IOException when the file cannot be found or any issues with the i/o occur
+*/
+public List<String> readAllFiles(String fileNames[]) 
+  {
+	List<String> fileContents = new  ArrayList<String>();
+	return fileContents;
   }
 }
